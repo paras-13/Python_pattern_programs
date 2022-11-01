ff02409e6a94462b7711da2adb76cce6dc0ba614
Paras Upadhyay <paras13upadhyay@gmail.com>
2022-11-02 00:56:55 +0530

    Inverted pyramid or inverted equilateral triangle
    This code prints a pattern of inverted pyramid or inverted equilateral triangle.

---
+++ Inverted_pyramidal_equlateral_triangle.py
@@ -0,0 +1,18 @@
+#Pattern Problem 5
+# Resembles inverted equilateral traingle or pyramid.
+#Demo eg:
+"""
+* * * * *
+ * * * *
+  * * *  
+   * * 
+    *
+"""
+# Code -->
+rows = int(input("Enter the vertical height as number of rows"))
+for i in range(rows):
+    for j in range(i):    # Prints space
+        print(" ",end='')
+    for k in range(rows-i):       # Prints asterik
+        print("* ",end='')
+    print()
