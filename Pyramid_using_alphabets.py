daee5ad8903831759c78aaa970ae62bd5cf10b00
Paras Upadhyay <paras13upadhyay@gmail.com>
2022-11-03 23:45:38 +0530

    Pyramid using alphabets
    Program to print pattern of pyramid or equilateral triangle using alphabets in ascending and descending order.

---
+++ Pyramid_using_alphabets.py
@@ -0,0 +1,47 @@
+# Program to print a pattern of equilateral triangle or pyramid using numbers
+"""
+Using alphabets in ascending order.
+Demo eg:
+    A
+   B B
+  C C C
+ D D D D
+E E E E E
+"""
+print("----------------------------------------------------")
+print("In ascending order")
+print("----------------------------------------------------")
+
+#Code -->
+rows = int(input("Enter the vertical height as number of rows"))
+Ascii = 65
+for i in range(rows):
+    for j in range(rows-(i+1)):
+        print(" ",end = "")
+    for k in range(i+1):
+        print(f"{chr(Ascii)} ",end = "")
+    Ascii+=1
+    print()
+
+print("-------------------------------------------------")
+print("In descending order")
+print("-------------------------------------------------")
+"""
+Using alphabets in descending order
+Demo eg:
+
+E E E E E 
+ D D D D
+  C C C
+   B B
+    A
+"""
+# Code-->
+Ascii = 65
+for p in range(rows):
+    for q in range(p):
+        print(" ",end = "")
+    for r in range(rows):
+        print(f"{chr(Ascii)} ",end = "")
+    Ascii+=1
+    print()
