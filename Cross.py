c94d1906f58e225763ff6c0f5fc816d47950974b
Paras Upadhyay <paras13upadhyay@gmail.com>
2022-12-21 23:23:36 +0530

    Pattern program to print cross using aestrick

---
+++ Cross.py
@@ -0,0 +1,27 @@
+# Program to print a pattern of cross using pattern printing with aestrick *
+#Pattern 
+"""
+"""
+# Cose -->
+for i in range(5):
+	if i==0 or i==4:
+		for j in range(5):
+			if j ==0 or j ==4:
+				print('*',end='')
+			else:
+				print(' ',end='')
+		print()
+	elif i==1 or i==3:
+		for j in range(5):
+			if j==1 or j==3:
+				print('*',end='')
+			else:
+				print(' ',end='')
+		print()
+	else:
+		for l in range(5):
+			if l==2:
+				print('*',end='')
+			else:
+				print(' ',end='')
+		print()
